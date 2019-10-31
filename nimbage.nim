94daa503d9aa879578c2f22bbf77a0a270d9c1ab
Barry DeZonia <bdezonia@gmail.com>
2019-10-31 17:52:23 -0500

    More playing with hierarchy

--- nimbage.nim
+++ nimbage.nim
@@ -1,14 +1,17 @@
 type
 
-  Algebra* = concept a, type T
-    a.construct() is T
-    a.construct(type T) is T
-    a.assign(type T, type var T)
+  Equality* = concept a, type T
     a.isEqual(type T, type T) is bool
     a.isNotEqual(type T, type T) is bool
     a.zero(type var T)
     a.isZero(type T) -> bool
-    
+
+  Algebra* = concept a, type T
+    a is Equality
+    a.construct() is T
+    a.construct(type T) is T
+    a.assign(type T, type var T)
+
   AdditiveGroup* = concept a, type T
     a is Algebra
     a.add(type T, type T, type var T)
