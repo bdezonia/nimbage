a7d3cd791429188c47592cd30e19d6575c2ee09c
Barry DeZonia <bdezonia@gmail.com>
2019-10-31 17:43:57 -0500

    Make some more type params into var type params

--- nimbage.nim
+++ nimbage.nim
@@ -6,11 +6,11 @@ type
     a.assign(type T, type var T)
     a.isEqual(type T, type T) is bool
     a.isNotEqual(type T, type T) is bool
-    a.zero(type T)
+    a.zero(type var T)
     a.isZero(type T) -> bool
     
   AdditiveGroup* = concept a, type T
     a is Algebra
-    a.add(type T, type T, type T)
-    a.subtract(type T, type T, type T)
-    a.negate(type T, type T)
+    a.add(type T, type T, type var T)
+    a.subtract(type T, type T, type var T)
+    a.negate(type T, type var T)
