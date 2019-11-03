4f6300e49d56b6da4740272e44485c053149fb9e
Barry DeZonia <bdezonia@gmail.com>
2019-11-03 12:54:07 -0600

    More type hacking

--- nimbage.nim
+++ nimbage.nim
@@ -1,5 +1,10 @@
 type
 
+  DefaultConstructible* = concept a, type T
+    a.construct() is T
+    a.construct(type T) is T
+    a.construct(string) is T
+  
   Equality* = concept a, type T
     a.isEqual(type T, type T) is bool
     a.isNotEqual(type T, type T) is bool
@@ -7,10 +12,7 @@ type
     a.isZero(type T) is bool
 
   Algebra* = concept a, type T
-    a is Equality
-    a.construct() is T
-    a.construct(type T) is T
-    a.construct(string) is T
+    a is DefaultConstructible and Equality
     a.assign(type T, type var T)
 
   AdditiveGroup* = concept a, type T
@@ -29,4 +31,15 @@ type
     
   RingWithUnity* = concept a, type T
     a is Ring and Unity
+ 
+#  Ints = RingWithUnity[int]
     
+
+#template add[T](a: T, b: T, c: var T) =
+#  c = a + b
+#  
+#var
+#  tmp
+#  
+#add(1, 3, tmp)
+#stdout.writeLine($tmp)
