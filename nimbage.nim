1c8f2fbc1ffefd6bf2ad688123895ae320d167ec
Barry DeZonia <bdezonia@gmail.com>
2019-11-04 20:55:09 -0600

    Test trial implementations of subbyte types
    Notice that you can't wrap values if assigned something outside.
    Might need to make interflow aware operators.

--- nimbage.nim
+++ nimbage.nim
@@ -43,3 +43,30 @@ type
 #  
 #add(1, 3, tmp)
 #stdout.writeLine($tmp)
+
+  UNum1 = range[uint8(0) .. uint8(1)]
+  UNum2 = range[uint8(0) .. uint8(3)]
+  UNum3 = range[uint8(0) .. uint8(7)]
+  UNum4 = range[uint8(0) .. uint8(15)]
+  UNum12 = range[uint16(0) .. uint16(4095)]
+  
+  SNum1 = range[int8(-1) .. int8(0)]
+  SNum2 = range[int8(-2) .. int8(1)]
+  SNum3 = range[int8(-4) .. int8(3)]
+  SNum4 = range[int8(-8) .. int8(7)]
+  SNum12 = range[int16(-2048) .. int16(2047)]
+
+var x: UNum2
+var y: UNum12
+var z: SNum12
+
+echo $sizeof(x)
+echo $sizeof(y)
+echo $sizeof(z)
+x = 0
+x = 1
+x = 2
+x = 3
+#x = 4
+#x = 5
+  
